86320d0449f104966274ebc80e1adc4b90b9f665
Daniele <daniele.dirollo@gmail.com>
2023-07-07 01:39:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+AOS.init({
+  duration: 1000,
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var typed = new Typed('#typed', {
+    strings: ["Intelligenza Artificiale", "Machine Learning", "Deep Learning", "Reti Neurali", "NLP", "Computer Vision"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true
+  });
+});

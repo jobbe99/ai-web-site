499cd14301e2b62bd4ede308b94ba115a53a2a54
Daniele <daniele.dirollo@gmail.com>
2023-07-07 01:49:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-AOS.init({
-  duration: 1000,
-});
-
+// Inserisci qui il codice JavaScript che gestisce la scrittura delle parole
 document.addEventListener('DOMContentLoaded', function() {
   var typed = new Typed('#typed', {
     strings: ["Intelligenza Artificiale", "Machine Learning", "Deep Learning", "Reti Neurali", "NLP", "Computer Vision"],
